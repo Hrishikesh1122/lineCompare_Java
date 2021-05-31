f503539b64a97e309a44298825da0e45f3f868ec
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-05-31 15:22:31 +0530

    Added UC1 : Distance bt 2 points

--- src/lineCompare/LineCompare.java
+++ src/lineCompare/LineCompare.java
@@ -1,10 +1,17 @@
 package lineCompare;
-
+import java.lang.Math;
 public class LineCompare {
+	
+	public static double dist(int x1,int y1,int x2,int y2) {
+		double length;
+		length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		return length;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to line comparison computation program");
-
+	   
+        System.out.println(dist(1,2,3,-4));
 	}
 
 }
