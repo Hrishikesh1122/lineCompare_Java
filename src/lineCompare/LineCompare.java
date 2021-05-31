02d6927fd99bd2bd51f3ca1946d5700faf11a4ab
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-05-31 16:08:31 +0530

    Added UC2 : check if lines are equal

--- src/lineCompare/LineCompare.java
+++ src/lineCompare/LineCompare.java
@@ -7,11 +7,20 @@ public class LineCompare {
 		length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
 		return length;
 	}
+	
+	public static void checkLength() {
+		double line1=dist(1,2,3,-4);
+		double line2=dist(1,-2,3,-4);
+		if(line1==line2)
+		     System.out.println("Lines are equal");
+		else
+			System.out.println("Lines not equal");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to line comparison computation program");
-	   
         System.out.println(dist(1,2,3,-4));
-	}
+        checkLength();
+    }
 
 }
